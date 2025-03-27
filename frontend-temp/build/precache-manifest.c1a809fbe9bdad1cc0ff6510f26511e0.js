5d63838fc46355912d833cbfd63660641abb66a8
veda-aarushi <121644311+veda-aarushi@users.noreply.github.com>
2025-03-27 16:07:15 -0700

    Add frontend after resolving submodule issue

---
+++ frontend-temp/build/precache-manifest.c1a809fbe9bdad1cc0ff6510f26511e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dddccf6457adda73caab245f60a01f52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc91ae81891302a40bd7",
+    "url": "/static/css/main.8a28bd04.chunk.css"
+  },
+  {
+    "revision": "0116abce3f6cbce16531",
+    "url": "/static/js/2.df6b8b77.chunk.js"
+  },
+  {
+    "revision": "4cb447811eafcde62d61d67183d8b566",
+    "url": "/static/js/2.df6b8b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e26406a4eeb9f55fec",
+    "url": "/static/js/3.028da947.chunk.js"
+  },
+  {
+    "revision": "fc91ae81891302a40bd7",
+    "url": "/static/js/main.c920c8b8.chunk.js"
+  },
+  {
+    "revision": "8b4ab0d508ee36357253",
+    "url": "/static/js/runtime-main.e26caa82.js"
+  }
+]);
